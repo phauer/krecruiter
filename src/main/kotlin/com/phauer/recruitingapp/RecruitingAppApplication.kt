664f472c3c31c0d7665a4d3adf49d206d3e22b17
Philipp Hauer <info@philipphauer.de>
2019-10-03 12:18:02 +0200

    remove vaadin

--- src/main/kotlin/com/phauer/recruitingapp/RecruitingAppApplication.kt
+++ src/main/kotlin/com/phauer/recruitingapp/RecruitingAppApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinRecruitingappApplication
+class KRecruiterApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinRecruitingappApplication>(*args)
+    runApplication<KRecruiterApplication>(*args)
 }
