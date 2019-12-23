ff52a550b5b8c55114c1f590d8b198e1807f9c6d
Philipp Hauer <info@philipphauer.de>
2019-12-23 14:46:24 +0100

    simplify PostgreSQLInstance.kt

--- src/test/kotlin/com/phauer/krecruiter/PostgreSQLInstance.kt
+++ src/test/kotlin/com/phauer/krecruiter/PostgreSQLInstance.kt
@@ -5,36 +5,31 @@ import org.postgresql.ds.PGSimpleDataSource
 import org.testcontainers.containers.PostgreSQLContainer
 import java.net.InetSocketAddress
 import java.net.Socket
-
-
-private const val localHost = "localhost"
-private const val localPort = 6000
+import javax.sql.DataSource
 
 object PostgreSQLInstance {
 
-    val dataSource by lazy {
-        if (isRunningLocally()) {
-            PGSimpleDataSource().apply {
-                setUrl("jdbc:postgresql://${localHost}:${localPort}/krecruiter")
-                user = "user"
-                password = "password"
-            }
-        } else {
-            val db = KPostgreSQLContainer("postgres:11.2-alpine")
-            db.start()
-            PGSimpleDataSource().apply {
-                setUrl(db.jdbcUrl)
-                user = db.username
-                password = db.password
-            }
+    val jdbi by lazy { Jdbi.create(createDataSource()).installPlugins() }
+
+    private fun createDataSource(): DataSource = if (isRunningLocally("localhost", 6000)) {
+        PGSimpleDataSource().apply {
+            setUrl("jdbc:postgresql://localhost:6000/krecruiter")
+            user = "user"
+            password = "password"
+        }
+    } else {
+        val db = KPostgreSQLContainer("postgres:11.2-alpine")
+        db.start()
+        PGSimpleDataSource().apply {
+            setUrl(db.jdbcUrl)
+            user = db.username
+            password = db.password
         }
     }
 
-    val jdbi by lazy { Jdbi.create(dataSource).installPlugins() }
-
-    private fun isRunningLocally() = try {
+    private fun isRunningLocally(host: String, port: Int) = try {
         val socket = Socket()
-        socket.connect(InetSocketAddress(localHost, localPort), 100)
+        socket.connect(InetSocketAddress(host, port), 100)
         socket.close()
         true
     } catch (exception: Exception) {
