822ae8b444b0efb6e43118dad003bdd3e19cff0c
Philipp Hauer <info@philipphauer.de>
2020-04-25 14:45:06 +0200

    remove redundant tests

--- src/test/kotlin/com/phauer/krecruiter/applicationApi/ApplicationControllerKotest.kt
+++ src/test/kotlin/com/phauer/krecruiter/applicationApi/ApplicationControllerKotest.kt
@@ -26,14 +26,12 @@ import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldContainIgnoringCase
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
-import okhttp3.mockwebserver.MockResponse
 import org.jdbi.v3.sqlobject.kotlin.onDemand
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.post
@@ -91,42 +89,7 @@ class ApplicationControllerKotest : FreeSpec() {
                 )
             }
 
-            "filter by application state" {
-                insertApplicationWithApplicant(id = 100, state = ApplicationState.REJECTED)
-                insertApplicationWithApplicant(id = 200, state = ApplicationState.REJECTED)
-                insertApplicationWithApplicant(id = 300, state = ApplicationState.INVITED_TO_INTERVIEW)
-                insertApplicationWithApplicant(id = 400, state = ApplicationState.EMPLOYED)
-                insertApplicationWithApplicant(id = 500, state = ApplicationState.RECEIVED)
-
-                val actualResponseDTO = mvc.requestApplications(state = ApplicationState.REJECTED)
-
-                actualResponseDTO.asClue {
-                    it.map(ApplicationDTO::id).shouldContainExactly(100, 200)
-                }
-            }
-
-            "return all when application state is not set" {
-                insertApplicationWithApplicant(id = 100, state = ApplicationState.REJECTED)
-                insertApplicationWithApplicant(id = 200, state = ApplicationState.INVITED_TO_INTERVIEW)
-
-                val actualResponseDTO = mvc.requestApplications(state = null)
-
-                actualResponseDTO.asClue {
-                    it.map(ApplicationDTO::id).shouldContainExactly(100, 200)
-                }
-            }
-
-            "order by dateCreated" {
-                insertApplicationWithApplicant(id = 100, dateCreated = 100.toInstant())
-                insertApplicationWithApplicant(id = 200, dateCreated = 200.toInstant())
-                insertApplicationWithApplicant(id = 300, dateCreated = 3.toInstant())
-
-                val actualResponseDTO = mvc.requestApplications()
-
-                actualResponseDTO.asClue {
-                    it.map(ApplicationDTO::id).shouldContainExactly(300, 100, 200)
-                }
-            }
+            // other tests coming here...
         }
         "Create Application" - {
             "posting an application creates an application and an applicant entry in the database with the posted values and the current timestamp" {
@@ -193,25 +156,6 @@ class ApplicationControllerKotest : FreeSpec() {
                 }
             }
 
-            "reject application with invalid address" {
-                validationService.enqueueValidationResponse(code = 200, valid = false)
-                val requestApplication = createApplicantEntity()
-
-                val response = postApplicationAndGetResponse(requestApplication)
-
-                response.status shouldBe 400
-                response.contentAsString shouldContainIgnoringCase "invalid address"
-            }
-
-            "return server error if the address validation service returns 500" {
-                validationService.enqueue(MockResponse().setResponseCode(500))
-                val requestApplication = createApplicantEntity()
-
-                val response = postApplicationAndGetResponse(requestApplication)
-
-                response.status shouldBe 500
-            }
-
             "dont create an application and return a 400 if an required JSON field is missing." - {
                 forAll( // there are two forall methods. import the one in the suspend package
                     row(MissingFieldApplicationDTO(firstName = "name1", lastName = "name2", street = "street", city = "city", jobTitle = null)),
@@ -244,6 +188,8 @@ class ApplicationControllerKotest : FreeSpec() {
                     }
                 }
             }
+
+            // other test coming here...
         }
     }
 
